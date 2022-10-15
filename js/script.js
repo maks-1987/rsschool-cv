ea466085fe4dc69e00b88984a56c94da934e4d72
MaxD <max.moderation@gmail.com>
2022-10-15 22:16:15 +0300

    feat: add links prog & skills

--- js/script.js
+++ js/script.js
@@ -20,26 +20,3 @@
         }
     }
 }); */
-
-// Adaptive menu
-/* const toggleBtn = document.querySelector('.burger-menu');
-const nav = document.querySelector('.burger-menu__button');
-const navList = document.querySelector('.burger-menu__list');
-const navItems = document.querySelectorAll('.nav-item');
-
-function switchMenu() {
-  toggleBtn.classList.toggle('collapsed');
-  nav.classList.toggle('collapsed');
-  navList.classList.toggle('collapsed');
-}
-toggleBtn.addEventListener('click', switchMenu);
-
-function closeMenu() {
-  setTimeout(() => {
-    toggleBtn.classList.remove('collapsed');
-    nav.classList.remove('collapsed');
-    navList.classList.remove('collapsed');
-  }, 1000);
-}
-navItems.forEach((el) => el.addEventListener('click', closeMenu));
- */
